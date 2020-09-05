5d14e59388cdc2dc65a85524da4e06dcf0a16af0
Никита Свечков <comradise@mail.ru>
2020-09-05 14:54:08 +0400

    DataTypes

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 2;
+            Console.WriteLine(a);
+            System.Int16 b = 2;
+            Console.WriteLine(a);
+        }
+    }
+}
