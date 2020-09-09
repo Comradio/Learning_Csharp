75ff0b8d44bb9bda11a56167e48d074209398f61
Никита Свечков <comradise@mail.ru>
2020-09-09 08:17:20 +0400

    Бинарные операции

---
+++ Arithmetic operations/Arithmetic operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 8;
+
+            double result = (double)a / b;
+            Console.WriteLine(result);         
+        }
+    }
+}
