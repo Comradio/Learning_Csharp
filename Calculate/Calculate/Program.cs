a27ce0bd84a8c9b5d1a6f223fdbe380451a23f22
Никита Свечков <comradise@mail.ru>
2020-09-13 16:01:57 +0400

    Calc

---
+++ Calculate/Calculate/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, result;
+            string act = "" ;
+
+            a = float.Parse(Console.ReadLine());
+            ConsoleKey consoleKey = Console.ReadKey().Key;
+
+            switch (consoleKey)
+            {
+                case ConsoleKey.Multiply:
+                    act = "*";
+                    break;
+                case ConsoleKey.Add:
+                    act = "+";
+                    break;
+                case ConsoleKey.Subtract:
+                    act = "-";
+                    break;
+                case ConsoleKey.Divide:
+                    act = "/";
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine();
+            b = float.Parse(Console.ReadLine());
+
+            switch (act)
+            {
+                case "*":
+                    result = a * b;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "+":
+                    result = a + b;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "-":
+                    result = a - b;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "/":
+                    result = a / b;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
