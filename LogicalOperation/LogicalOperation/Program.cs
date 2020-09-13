5c7742393b15ec80279f8620256f77d276d14bd5
Никита Свечков <comradise@mail.ru>
2020-09-13 09:36:04 +0400

    Processor Temperature

---
+++ LogicalOperation/LogicalOperation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalOperation
+{
+    class Program
+    {
+        
+            public static bool GetTemperature()
+            {
+                return true;
+            }
+
+            public static bool GetCoolingStatus()
+            {
+                return true;
+            }
+
+        static void Main(string[] args)
+        {
+            if (GetTemperature() && GetCoolingStatus())
+            {
+                Console.WriteLine("Угроза повреждения процессора!");
+            }
+
+            // Более параноидальная версия
+            if (GetTemperature() && GetCoolingStatus())
+            {
+                Console.WriteLine("Угроза повреждения процессора!");
+            }
+
+        }
+    }
+}
