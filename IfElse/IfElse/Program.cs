7ca4e25aad16237757967b6d5471da6baece3cff
Никита Свечков <comradise@mail.ru>
2020-09-13 09:15:30 +0400

    Is infected?

---
+++ IfElse/IfElse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isInfected;
+            float temp;
+
+            Console.WriteLine("Введите значение температуры тела персонажа: ");
+            temp = float.Parse(Console.ReadLine());
+
+            if (temp > 37.2)
+            {
+                isInfected = true;
+            }
+            else
+            {
+                isInfected = false;
+            }
+
+            if (isInfected)
+            {
+                Console.WriteLine("Персонаж болен!");
+            }
+            else 
+            {
+                Console.WriteLine("Персонаж здоров!");
+            }
+        }
+    }
+}
