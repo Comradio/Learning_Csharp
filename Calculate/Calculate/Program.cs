3addcabdf37e714d509f1e29d0cb4f7f606c5c17
Никита Свечков <comradise@mail.ru>
2020-09-13 16:17:36 +0400

    And some more

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -13,63 +13,86 @@ namespace Calculate
             float a, b, result;
             string act = "" ;
 
-            Console.WriteLine("Введите первое число: ");
-            a = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Выберите действие: +, -, *, /");
-            ConsoleKey consoleKey = Console.ReadKey().Key;
-                        
-            switch (consoleKey)
+            while (true)
             {
-                case ConsoleKey.Multiply:
-                    act = "*";
-                    break;
-                case ConsoleKey.Add:
-                    act = "+";
-                    break;
-                case ConsoleKey.Subtract:
-                    act = "-";
-                    break;
-                case ConsoleKey.Divide:
-                    act = "/";
-                    break;
-                default:
-                    break;
-            }
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Введите первое число: ");
+                    a = float.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введено не число!");
+                    Console.ReadKey();
+                    continue;
+                }
 
-            Console.WriteLine();
-            Console.WriteLine("Введите второе число: ");
-            b = float.Parse(Console.ReadLine());
+                Console.WriteLine("Выберите действие: +, -, *, /");
+                ConsoleKey consoleKey = Console.ReadKey().Key;
 
-            switch (act)
-            {
-                case "*":
-                    result = a * b;
-                    Console.WriteLine("Результат: " + result);
-                    break;
-                case "+":
-                    result = a + b;
-                    Console.WriteLine("Результат: " + result);
-                    break;
-                case "-":
-                    result = a - b;
-                    Console.WriteLine("Результат: " + result);
-                    break;
-                case "/":
-                    if (b == 0)
-                    {
-                        Console.WriteLine("На ноль делить здесь не положено!");
-                        result = 0;
-                    }
-                    else
-                    {
-                        result = a / b;
+                switch (consoleKey)
+                {
+                    case ConsoleKey.Multiply:
+                        act = "*";
+                        break;
+                    case ConsoleKey.Add:
+                        act = "+";
+                        break;
+                    case ConsoleKey.Subtract:
+                        act = "-";
+                        break;
+                    case ConsoleKey.Divide:
+                        act = "/";
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+                try
+                {
+                    Console.WriteLine("Введите второе число: ");
+                    b = float.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введено не число!");
+                    Console.ReadKey();
+                    continue;
+                }
+                
+                switch (act)
+                {
+                    case "*":
+                        result = a * b;
                         Console.WriteLine("Результат: " + result);
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Была введена неверная операция!");
-                    break;
+                        break;
+                    case "+":
+                        result = a + b;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "-":
+                        result = a - b;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "/":
+                        if (b == 0)
+                        {
+                            Console.WriteLine("На ноль делить здесь не положено!");
+                            result = 0;
+                        }
+                        else
+                        {
+                            result = a / b;
+                            Console.WriteLine("Результат: " + result);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Была введена неверная операция!");
+                        break;
+                }
+                Console.ReadKey();
             }
         }
     }
