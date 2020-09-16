c32b968f2d38b129b9c1eeb2c588dcf9853437d9
Никита Свечков <comradise@mail.ru>
2020-09-16 07:22:46 +0400

    While

---
+++ While/While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit, count = 0;
+
+            limit = int.Parse(Console.ReadLine());
+
+            while (count < limit)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
