aff895283e0b401e11496a37e9f6a1f16728d47a
Никита Свечков <comradise@mail.ru>
2020-10-01 11:15:39 +0400

    Embedded cycles + rectangle

---
+++ Embedded cycles/Embedded cycles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embedded_cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("Цикл 1, итерация № " + i);
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("\tцикл 2, итерация № " + j);
+                    for (int k = 0; k < 2; k++)
+                    {
+                        Console.WriteLine("\t\tцикл 3, итерация № " + k);
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Embedded cycles/Rectangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            int rows, cols;
+            ch = char.Parse(Console.ReadLine());
+            rows = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(ch);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
