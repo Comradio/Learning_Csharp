8f4166b8d31b2a41d3e07e72f2e0bf6181a83a18
Никита Свечков <comradise@mail.ru>
2020-11-14 08:28:03 +0400

    Array[5]

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray;
+
+            myArray = new int[5];
+
+            myArray[0] = 10;
+            myArray[1] = 3;
+
+            int a = myArray[0];
+
+            Console.WriteLine(a);
+            Console.WriteLine(myArray.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
