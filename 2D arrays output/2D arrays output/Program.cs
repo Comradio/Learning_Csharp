9a6f46d2c04070cd34e99cafc649a84a740a575e
Никита Свечков <comradise@mail.ru>
2021-03-08 12:24:28 +0400

    Input from random and from keyboard

---
+++ 2D arrays output/2D arrays output/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _2D_arrays_output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[2, 3];
+            Random random = new Random();
+
+            /*for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = random.Next();
+                }
+            }*/
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine($"I: {i} J: {j}");
+                    myArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
