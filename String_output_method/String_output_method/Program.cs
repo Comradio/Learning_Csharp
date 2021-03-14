e82893eafe59399445c60c2f63cc89805ec3724e
Никита Свечков <comradise@mail.ru>
2021-03-14 10:45:46 +0400

    Program prints string of users char

---
+++ String_output_method/String_output_method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace String_output_method
+{
+    class Program
+    {
+        static void PrintLine(char symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ: ");
+            char symbol = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.WriteLine("Введите количеств осимволов: ");
+            uint symbolsCount = uint.Parse(Console.ReadLine());
+
+            PrintLine(symbol, symbolsCount);
+            Console.ReadKey();
+        }
+    }
+}
