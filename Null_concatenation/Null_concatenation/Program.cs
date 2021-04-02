23e06a5b7c40ac4c04019100c527fd34e38c6c89
Никита Свечков <comradise@mail.ru>
2021-04-02 08:31:05 +0400

    Evasion_null

---
+++ Null_concatenation/Null_concatenation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Null_concatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            // <проверяемая переменная> ?? <дефолтное значение>
+            // проверяемая переменная и дефолтное значение должны быть одного типа данных
+            string result = str ?? "нет данных";
+
+            Console.WriteLine(str ?? "нет данных");
+            Console.WriteLine(result);
+
+            string result1 = str ?? "";
+            string result2 = str ?? string.Empty;
+
+            Console.WriteLine("количество символов в строке: " + result1.Length);
+            Console.WriteLine("количество символов в строке: " + result2.Length);
+        }
+    }
+}
