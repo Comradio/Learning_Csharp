7c38e854fa44ceffabcb9c4a8166ff6975f8337a
Никита Свечков <comradise@mail.ru>
2021-07-26 08:17:16 +0400

    Оператор объединения присваивания с null (??=)

---
+++ join_assignment_with_null/join_assignment_with_null/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace join_assignment_with_null
+{
+    class Program
+    {
+        static int[] GetArray()
+        {
+            int[] myArray = null;
+
+            return myArray;
+        }
+        static void Main(string[] args)
+        {
+            string str = null;
+            str ??= string.Empty;
+            Console.WriteLine("Количество символов в строке: " + str.Length);
+            
+            int[] myArray = GetArray();
+            myArray ??= new int[0];
+            Console.WriteLine("Количество элементов в массиве: " + myArray.Length);
+        }
+    }
+}
