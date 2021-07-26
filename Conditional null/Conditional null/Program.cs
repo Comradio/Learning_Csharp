b8e15dd333c5d83a4c2a76e95908b6ad6a5c4643
Никита Свечков <comradise@mail.ru>
2021-07-26 08:42:00 +0400

    Оператор условного null?.

---
+++ Conditional null/Conditional null/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+/*  ?.  */
+
+namespace Conditional_null
+{
+    class Program
+    {
+        static int[] GetArray()
+        {
+            int[] myArray = null;
+
+            return myArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = GetArray();
+            Console.WriteLine("Сумма элементов массива: " + (myArray?.Sum() ?? 0));
+        }
+    }
+}
